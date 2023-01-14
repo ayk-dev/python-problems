8e27c9361a7611ace53c06d8d7a0e4fb3c57c2ba
Albena Krasteva <albena.y.krasteva@gmail.com>
2023-01-14 15:53:24 -0600

    Functions added

--- 100daysOfCode/coffee-machine-project/main.py
+++ 100daysOfCode/coffee-machine-project/main.py
@@ -6,9 +6,25 @@ def report(resrcs, money):
     return result
 
 
-def are_resources_sufficient(choice, menu, resrcs):
-    for k, v in resrcs.items():
-        pass
+def resources_insufficient(choice, menu, resrcs):
+    for ingr in menu[choice]["ingredients"]:
+        # print(f'ingr {ingr}')
+        # print(f'menu[choice]["ingredients"] {menu[choice]["ingredients"]}')
+        # print(f'menu[choice]["ingredients"][ingr] {menu[choice]["ingredients"][ingr]}')
+        # print(resrcs[ingr])
+        if menu[choice]["ingredients"][ingr] >= resrcs[ingr]:
+            return ingr
+    return False
+
+
+def process_money(qs, ds, ns, ps, menu, choice):
+    """If inserted money by user is enough, returns change, else return None if money is refunded"""
+    inserted_money = (qs * 25 + ds * 10 + ns * 5 + ps * 1) / 100
+    if inserted_money >= menu[choice]["cost"]:
+        change = inserted_money - menu[choice]["cost"]
+        return change
+    return None
+
 
 
 MENU = {
@@ -44,6 +60,8 @@ resources = {
 }
 
 money = 0
+
+
 while True:
     user_input = input("What would you like? (espresso/latte/cappuccino):").lower()
 
@@ -53,5 +71,18 @@ while True:
 
     if user_input == 'off':
         break
+
+    if not resources_insufficient(user_input, MENU, resources):
+        print('Please insert coins.')
+        quaters = int(input("How many quarters?:"))
+        dimes = int(input("How many dimes?:"))
+        nickels = int(input("How many nickels?:"))
+        pennies = int(input("How many pennies?:"))
+        process_money(quaters, dimes, nickels, pennies, MENU, user_input)
+        #TODO complete process money
+        #TODO prepare drink
+    else:
+        print(f'Sorry, there is not enough {resources_insufficient(user_input, MENU, resources)}')
+
 
 
