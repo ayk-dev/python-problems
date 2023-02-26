dacbad53fb13a98e1b2588074df4e6cb9cdf43e9
Albena Krasteva <albena.y.krasteva@gmail.com>
2023-02-25 22:15:33 -0600

    Add collision functionality

--- snake_game/snake_main.py
+++ snake_game/snake_main.py
@@ -1,6 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 scr = Screen()
 scr.setup(width=600, height=600)
@@ -9,6 +10,7 @@ scr.title("Snake Game")
 scr.tracer(0)
 
 snake = Snake()
+food = Food()
 
 scr.listen()
 scr.onkey(snake.up, "Up")
@@ -21,6 +23,9 @@ while game_on:
     scr.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 scr.exitonclick()
